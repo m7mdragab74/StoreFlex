0d70cee370a0f033125377a26c155d45498c3a8e
m7mdragab <mr341349@gmail.com>
2024-10-16 16:00:32 +0300

    try to use post request

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          FloatingActionButton(
+            onPressed: () async {
+              http.Response response = await http
+                  .post(Uri.parse('https://fakestoreapi.com/products'), body: {
+                'title': 'test product',
+                'price': '13.5',
+                'description': 'lorem ipsum se\'t',
+                'image': 'https://i.pravatar.cc',
+                'category': 'electronic'
+              }, headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/x-www-form-urlencoded',
+              });
+              print(response.body);
+            },
+            child: Icon(Icons.add),
+          )
+        ],
+      ),
+    );
   }
 }
